35deabbc9cc8ff82552106e053cda4c57a521095
planwis <witch2544@hotmail.co.th>
2022-07-15 15:44:14 +0700

    init base test

---
+++ test/foudry/unit/base/BaseTest.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+
+abstract contract BaseTest is Test {
+  address public constant ALICE = address(1);
+  address public constant BOB = address(2);
+  address public constant CATHY = address(3);
+  address public constant EVE = address(4);
+
+  ProxyAdmin internal proxyAdmin;
+
+  constructor() {
+    proxyAdmin = _setupProxyAdmin();
+  }
+
+  function _setupProxyAdmin() internal returns (ProxyAdmin) {
+    return new ProxyAdmin();
+  }
+}
